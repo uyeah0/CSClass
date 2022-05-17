0a3d3951f8c958042ff145c16e9c8fe4e6d038d4
s2031@e-mirim.hs.kr <s2031@e-mirim.hs.kr>
2022-05-17 11:56:19 +0900

    basic class : 사용자 정의 타입 Car 클래스 제작 추가로 // xml 메서드 주석도 배움 (#1)

---
+++ CSClass/CSClass/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSClass
+{
+    internal class Car
+    {
+        // carNumber : 자동차 번호
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        /// <summary>
+        /// 자동차가 들어온 시간 설정
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        internal void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// 자동차가 나간 시간 설정
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        internal void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
+
+        
+    }
+}
