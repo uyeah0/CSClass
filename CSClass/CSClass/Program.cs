314921e0ecb4f19df853bc29e7c547801f218835
s2031@e-mirim.hs.kr <s2031@e-mirim.hs.kr>
2022-05-30 14:44:43 +0900

    basic class : List 클래스 (#2)

--- CSClass/CSClass/Program.cs
+++ CSClass/CSClass/Program.cs
@@ -28,6 +28,21 @@ namespace CSClass
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble() * 10);
 
+            /*
+            List<int> list = new List<int>();
+            list.Add(59);
+            list.Add(3);
+            list.Add(100);
+            list.Add(11);
+            */
+
+            List<int> list = new List<int>() { 59, 3, 100, 11 };
+            list.Remove(42); // 숫자로 찾기
+            list.RemoveAt(0); // 인덱스로 찾기
+            foreach (var item in list)
+            {
+                Console.WriteLine("Count : " + list.Count + "\t" + item);
+            }
         }
     }
 }
