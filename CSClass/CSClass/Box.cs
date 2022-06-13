249b465f60755a57fdb1032edf0be5c80dd8d597
s2031@e-mirim.hs.kr <s2031@e-mirim.hs.kr>
2022-06-13 15:04:44 +0900

    method : 22-8. 속성 property (#14) C#의 유용한 문법 getter, setter를 간단한 코드로 작성하기! prop, propfull을 이용하여 getter setter 생성

---
+++ CSClass/CSClass/Box.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace CSClass
+{
+    public class Box
+    {
+        
+        public int foo { get; set; } // 기본적인 getter, setter
+
+        private int width;
+
+        public int Width
+        {
+            // get, set 생략 가능
+            get { return width; }
+            set
+            {
+                if (value > 0)
+                {
+                    width = value;
+                }
+                else
+                {
+                    Console.WriteLine("Box의 가로는 양수여야 합니다.");
+                }
+            }
+        }
+        // prop 간략하게 propfull 전체적 tab키
+
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    Console.WriteLine("Box의 세로는 양수여야 합니다.");
+                }
+            }
+        }
+
+
+
+
+
+        public Box(int w, int h)
+        {
+            this.Width = w;
+            this.Height = h;
+        }
+
+        private int area;
+
+        public int Area
+        {
+            get { return this.width * this.height; }
+        }
+    }
+}
--- CSClass/CSClass/Program.cs
+++ CSClass/CSClass/Program.cs
@@ -139,6 +139,16 @@ namespace CSClass
             Product product = new Product("과자", 1500);
             productA = null;
 
+            Box box = new Box(10, 20);
+            box.Width =200;
+            box.Height = 300;
+            Console.WriteLine(box.Area);
+
+            box.Width = -10;
+            Console.WriteLine(box.Area);
+
+            Box box2 = new Box(-10, -20);
+            Console.WriteLine(box2.Area);
         }
     }
 }
